a10933bf95ca86e4f5b9a1e0661fde3955d62eac
amaury <amaurypb845@gmail.com>
2026-01-05 16:51:23 -0400

    Nova alteração

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-
-
-import MoiPerfil from './assets/moi-perfil.png';
-; // caminho relativo do componente pra imagem
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <React.StrictMode>
-    <div style={{ textAlign: 'center' }}>
-      <img src={MoiPerfil} alt="Dra. Andréia" style={{ width: '250px', borderRadius: '50%' }} />
-      <App />
-    </div>
-  </React.StrictMode>
-);
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
