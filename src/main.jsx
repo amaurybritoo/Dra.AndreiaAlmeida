8df0a0cbcc2ebffb948695ec0626841ac8d46fb5
amaury <amaurypb845@gmail.com>
2026-01-05 16:47:41 -0400

    Nova alteração

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,20 @@ import './index.css'
 import App from './App.jsx'
 
 
-import FotoAndreia from './assets/moi-perfil.png';
 
-function Sobre() {
-  return (
-    <div>
-      <img src={FotoAndreia} alt="Dra. Andréia Almeida" />
+import MoiPerfil from './assets/moi-perfil.png';
+; // caminho relativo do componente pra imagem
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <div style={{ textAlign: 'center' }}>
+      <img src={MoiPerfil} alt="Dra. Andréia" style={{ width: '250px', borderRadius: '50%' }} />
+      <App />
     </div>
-  );
-}
+  </React.StrictMode>
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
